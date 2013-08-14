cf68a16aae161976d8e254e5b75b4e6f396bdd5e
Victor Espinosa <im_vicoy@hotmail.com>
2013-08-14 02:37:56 -0500

    ocho

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+	<head>
+			<meta charset="utf-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<title></title>
+			<link rel="stylesheet" href="css/style.css">
+			
+			<?php include('includes/scripts.inc') ?>
+
+
+	</head>
+	<body>
+		<div id="wnav1">
+						<div class="anchoPantalla" style="position:relative;">
+							<div id="logo">
+								<img src="img/logo.png">
+							</div>
+							<img src="img/head_1.png" style="margin-left:157px; margin-top:10px;">
+						</div>
+		</div>
+		<div id="wnav2">
+			<?php include('includes/nav.inc')?>
+		</div>
+		
+						<div id="wprincipal" style="">
+							<div class="anchoPantalla" id="principal" style="background:url(img/logote.jpg) right top no-repeat;">
+								
+
+							
+								<div class="col1">
+									<img src="img/home/video.png" style="cursor:pointer;" >
+								</div>
+
+								<hr>
+							
+								<div class="col3"> <img src="img/home/giftcard.png" alt="GIF CARD"></div>
+								<div class="col3 botonImg" style="height:310px; background:url(img/home/talentspot.jpg)" alt="talentspot"></div>
+								<div class="col3"><img src="img/home/noticias.jpg" alt="GIF CARD"></div>
+								
+							
+
+								<div class="dosCorta" ></div>
+								<div class="dosLarga" ></div>
+
+
+
+							
+
+							</div>
+
+
+							<div class="anchoPantalla" id="footer">
+								<img src="img/foot.png" >
+							</div>
+
+					  </div>
+	</body>
+</html>
